ea73d683fde691f2f88a7b2a330aba0166e7dcb5
Saint-loup <shakespearien@gmail.com>
2020-04-05 00:15:10 +0200

    ajout de raccourcis clavier

--- src/main.js
+++ src/main.js
@@ -4,8 +4,19 @@ import './registerServiceWorker'
 
 Vue.config.productionTip = true;
 Vue.config.devtools = true;
+import { store } from './store.js';
 
 
 new Vue({
   render: h => h(App),
+  methods: {
+	SpaceListener: function(evt) {
+	if (evt.code === "Space" || evt.code === "Enter") {
+		store.toggle()
+		}
+	}
+  },
+  created: function() {
+	document.addEventListener('keyup', this.SpaceListener);
+  },
 }).$mount('#app')
