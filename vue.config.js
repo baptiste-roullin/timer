896b92f4edd89d6d4a798822ad629f554d91850d
Baptiste <baptiste@roullin.net>
2021-11-24 23:40:36 +0100

    supprimé mode dev

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 module.exports = {
 
-  publicPath: process.env.NODE_ENV === 'dev'
-    ? '/'
-    : '/Timer',
+  publicPath: '/Timer',
   pwa: {
     themeColor: '#96b0ca',
     msTileColor: '#000000',
